d516638a605f5d091a226bc5efc829579f245f6d
Agshin Rajabov <akorecebov@gmail.com>
2019-12-10 15:23:50 +0400

    Fayllar

---
+++ app.js
@@ -0,0 +1,11 @@
+var i = 1;
+
+// while(i > 8) {
+//     document.write('Sayi: ' + i + '<br/>');
+//     i++;
+// }
+
+// do {
+//     document.write('Sayi: ' + i + '<br/>');
+//     i++;
+// } while(i >= 8);
